14ecb2c93be02c0e1ca79d803156213e99bf2b20
(cdep)illabout <cdep.illabout@gmail.com>
2016-12-09 00:17:23 +0900

    Add some tests to the session_info.ml file on creating modules.

--- code/files-modules-and-programs/session_info.ml
+++ code/files-modules-and-programs/session_info.ml
@@ -21,4 +21,24 @@ type session_info = { user: Username.t;
                     }
 
 let sessions_have_same_user s1 s2 =
-  s1.user = s2.host
+  (* This is incorrect: *)
+  (* s1.user = s2.host *)
+  Username.to_string s1.user = Hostname.to_string s2.host
+
+module What = struct
+  type t = string
+  let who = 3
+  let where = 4
+  let (ga : t) = "hehehe"
+end
+
+let lala =
+  What.who + What.where
+
+(* example of a local open *)
+let test =
+  let open What in
+  who + where
+
+let average x y =
+  Int64.(x + y / of_int 2)
