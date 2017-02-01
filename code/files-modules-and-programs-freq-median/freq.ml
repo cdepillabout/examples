e7e0b3e75df750076ec6f3350cafecadd4453497
(cdep)illabout <cdep.illabout@gmail.com>
2017-02-01 22:32:31 +0900

    Add merlin file and actually call print median.

--- code/files-modules-and-programs-freq-median/freq.ml
+++ code/files-modules-and-programs-freq-median/freq.ml
@@ -3,6 +3,21 @@ open Core.Std
 let build_counts () =
   In_channel.fold_lines stdin ~init:Counter.empty ~f:Counter.touch
 
+let print_top counter =
+  Counter.to_list counter
+  |> List.sort ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
+  |> (fun counts -> List.take counts 10)
+  |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
+
+let print_median counter = ()
+
+let () =
+  let counter = build_counts () in
+  print_top counter ;
+  (* TODO: Ask question on stack overflow about how to find functions based on
+   * names or type signatures. *)
+  Use_median_1.print_median (Counter.median counter)
+
 let () =
   build_counts ()
   |> Counter.to_list
