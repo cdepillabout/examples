b22fc1428b779fe8e1f154cc3fc9e00b91af8be7
(cdep)illabout <cdep.illabout@gmail.com>
2017-08-19 21:57:22 +0900

    Rename some functions.

--- code/guided-tour/weather_api/main.ml
+++ code/guided-tour/weather_api/main.ml
@@ -1,3 +1,1 @@
-let () =
-  (* print_endline Weather_api.message; *)
-  Weather_api.run ()
+let () = Weather_api.run ()
--- code/guided-tour/weather_api/weather_api.ml
+++ code/guided-tour/weather_api/weather_api.ml
@@ -2,11 +2,9 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let message = "Hello, world!"
-
 let weather_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22tokyo%2C%20japan%22)%20and%20u%20%3D%20%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 
-let body =
+let do_request =
   Client.get (Uri.of_string weather_url) >>= fun (resp, body) ->
   let code = resp |> Response.status |> Code.code_of_status in
   Printf.printf "Response code: %d\n" code;
@@ -16,5 +14,5 @@ let body =
   body
 
 let run () =
-  let body = Lwt_main.run body in
+  let body = Lwt_main.run do_request in
   print_endline ("Received body\n" ^ body)
