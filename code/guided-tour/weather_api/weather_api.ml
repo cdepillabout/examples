7dd86e821227298de6354d92aca7aee939063e11
(cdep)illabout <cdep.illabout@gmail.com>
2017-08-19 22:30:01 +0900

    Redo the changes to the example.

--- code/guided-tour/weather_api/weather_api.ml
+++ code/guided-tour/weather_api/weather_api.ml
@@ -2,11 +2,16 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let url = "https://google.com"
+let weather_url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22tokyo%2C%20japan%22)%20and%20u%20%3D%20%22c%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback="
 
 let do_request =
-  Client.get (Uri.of_string url) >>= fun (resp, body) ->
-  body |> Cohttp_lwt.Body.to_string
+  Client.get (Uri.of_string weather_url) >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
 
 let run () =
   let body = Lwt_main.run do_request in
