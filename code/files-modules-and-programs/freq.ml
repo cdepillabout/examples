03593fc85db2c2c225ae39338c6c3cae8004bf7a
(cdep)illabout <cdep.illabout@gmail.com>
2017-09-04 18:19:15 +0900

    Change freq.ml for new book.

--- code/files-modules-and-programs/freq.ml
+++ code/files-modules-and-programs/freq.ml
@@ -1,17 +1,22 @@
-open Core.Std
+open Core
 
-let build_counts () =
-  In_channel.fold_lines stdin ~init:[] ~f:(fun counts line ->
-    let count =
-      match List.Assoc.find counts line with
-      | None -> 0
-      | Some x -> x
-    in
-    List.Assoc.add counts line (count + 1)
-  )
+let build_counts () : (string, int) List.Assoc.t =
+  In_channel.fold_lines
+    In_channel.stdin
+    ~init:[]
+    ~f:(fun (counts : (string, int) List.Assoc.t) (line : string) ->
+        let count =
+          match List.Assoc.find counts line ~equal:String.equal with
+          | None -> 0
+          | Some x -> x
+        in
+        List.Assoc.add ~equal:String.equal counts line (count + 1)
+      )
 
-let () =
+let run () =
   build_counts ()
-  |> List.sort ~cmp:(fun (_,x) (_,y) -> Int.descending x y)
+  |> List.sort ~cmp:(fun (_,(x : int)) (_,(y : int)) -> Int.descending x y)
   |> (fun l -> List.take l 10)
   |> List.iter ~f:(fun (line,count) -> printf "%3d: %s\n" count line)
+
+let () = run ()
