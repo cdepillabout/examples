d20fab8e86e051c887739334a24355d5a83fddc5
(cdep)illabout <cdep.illabout@gmail.com>
2017-10-02 00:24:40 +0900

    Add some additional code for example.

--- code/variants/example.ml
+++ code/variants/example.ml
@@ -233,10 +233,6 @@ let res6 =
        )
     )
 
-
-(* TODO: project idea: play around with Core_kernel.Blang. *)
-
-
 let three : [> `Int of int] = `Int 3
 
 let four : [> `Float of float] = `Float 4.
@@ -274,3 +270,29 @@ let res8 =
         | Error _ -> false
         | Ok b -> b
       )
+
+module PolyVarColor = struct
+
+  let basic_color_to_int = function
+    | `Black -> 0 | `Red     -> 1 | `Green -> 2 | `Yellow -> 3
+    | `Blue  -> 4 | `Magenta -> 5 | `Cyan  -> 6 | `White  -> 7
+
+  let color_to_int = function
+    | `Basic (basic_color,weight) ->
+        let base = match weight with `Bold -> 8 | `Regular -> 0 in
+        base + basic_color_to_int basic_color
+    | `RGB (r,g,b) -> 16 + b + g * 6 + r * 36
+    | `Gray i -> 232 + i
+
+  let extended_color_to_int = function
+    | `RGBA (r,g,b,a) -> 256 + a + b * 6 + g * 36 + r * 216
+    (* Note that we can't use a catch-all case here, because then color_to_int
+     * wouldn't be passed something of the correct type. *)
+    | (`Basic _ | `RGB _ | `Gray _) as color -> color_to_int color
+
+  let extended_color_to_int' : [< `RGBA of int * int * int * int | `Basic of int * int | `RGB of int * int * int | `Gray of int ] = function
+    | `RGBA (r,g,b,a) -> 256 + a + b * 6 + g * 36 + r * 216
+    (* We can use a catch-all case here because the type signature limits what
+     * the type of color can be. *)
+    | color -> color_to_int color
+end
