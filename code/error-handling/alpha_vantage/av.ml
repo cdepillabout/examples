7f8a7d1edd09adb4c014412e1a4f858b7bcd38a9
(cdep)illabout <cdep.illabout@gmail.com>
2017-12-10 18:43:24 +0900

    Update.

--- code/error-handling/alpha_vantage/av.ml
+++ code/error-handling/alpha_vantage/av.ml
@@ -1,3 +1,16 @@
 open Core
 
-let () = printf "hello world\n"
+let api_key_env_var_name = "ALPHA_VANTAGE_API_KEY"
+
+let get_api_key =
+  match Sys.getenv "ALPHA_VANTAGE_API_KEY" with
+  | Some env_var -> env_var
+  | None ->
+    printf
+      "WARNING: You must specify the Alpha Vantage API Key in the environment \
+       variable \n\"%s\".  Since it is not specified, using the API Key \
+       \"demo\".\n"
+      api_key_env_var_name ;
+    "demo"
+
+let () = printf "%s\n" get_api_key
